683170f50845a8c511fd4a823ade1aacfdf933bd
Reid Miller <reid.s.miller@gmail.com>
2023-01-31 20:55:04 -0700

    Add card generator to flashcard_runner and test game

--- flash_cards/flashcard_runner.rb
+++ flash_cards/flashcard_runner.rb
@@ -2,14 +2,15 @@ require './lib/card'
 require './lib/deck'
 require './lib/round'
 require './lib/turn'
+require './lib/card_generator'
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-card_4 = Card.new("What is the oldest pokemon?", "Mew", :Pokemon)
-card_5 = Card.new("What is Reid's favorite pokemon?", "Lugia", :Pokemon)
+filename = ('./cards.txt')
+card_generator = CardGenerator.new(filename)
 
-deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
+card_generator.read_txt
+card_generator.convert_to_card
+
+deck = Deck.new(card_generator.cards)
 round = Round.new(deck)
 
 round.start
