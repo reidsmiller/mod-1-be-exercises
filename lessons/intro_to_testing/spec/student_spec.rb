c99e02c01cfd5d32235629cdba458ac0375f7403
Reid Miller <reid.s.miller@gmail.com>
2023-02-01 11:05:44 -0700

    Complete intro to testing lesson

--- lessons/intro_to_testing/spec/student_spec.rb
+++ lessons/intro_to_testing/spec/student_spec.rb
@@ -1,13 +1,30 @@
 # Add the 2 requires you will need here
+require 'rspec'
+require './lib/student'
 
+# Domain Specefic Language
 
-describe Student do
+RSpec.describe Student do
   describe '#initialize' do
     it 'is an instance of student' do
       # write the code to initialize a new student object
+      student1 = Student.new('Charlie')
+
+      expect(student1).to be_instance_of(Student)
     end
     # test it has a name
+    it 'has a name' do
+      student1 = Student.new('Charlie')
+
+      expect(student1.name).to eq('Charlie')
+    end
     # test it has cookies
+    it 'has cookies' do
+
+    end
     # test it can add cookies
+    it 'can add cookies' do
+      
+    end
   end
 end
